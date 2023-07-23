b25c53985c45cd06fb2ee96ca01d420c00430223
julius-alexander <jgarzonferrer@gmail.com>
2023-07-23 13:17:56 -0400

    Added content from episode 10 - CSS properties on index.css

--- first-web/src/Home.js
+++ first-web/src/Home.js
@@ -1,26 +1,22 @@
 import { useState } from 'react';
 
 const Home = () => {
-    // let name = 'mario';
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState(30);
-
-    const handleClick = () => {
-        if (name === 'Mario') {
-            setName('Luigi');
-            setAge(25);
-        }
-        else {
-            setName('Mario');
-            setAge(30);
-        }
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={ handleClick }>Click Me</button>
+            
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
+
         </div>
     );
 }
