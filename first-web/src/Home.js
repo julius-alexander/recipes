cb26ab765b9a6b2a037292128a29db68e8fd83ae
julius-alexander <jgarzonferrer@gmail.com>
2023-07-23 12:40:58 -0400

    Modified Home.js to cycle between "Mario is 30 years old" and "Luigi is 25 years old"#

--- first-web/src/Home.js
+++ first-web/src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(30);
+
+    const handleClick = () => {
+        if (name === 'Mario') {
+            setName('Luigi');
+            setAge(25);
+        }
+        else {
+            setName('Mario');
+            setAge(30);
+        }
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
+            <button onClick={ handleClick }>Click Me</button>
         </div>
     );
 }
