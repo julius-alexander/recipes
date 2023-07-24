b40caee92e592000e315c846dd6ae65318ead4b7
julius-alexander <jgarzonferrer@gmail.com>
2023-07-24 16:15:32 -0400

    - Added react-router-dom functions, using version 6.14 - Used 'Routes' to specify different paths - Used 'Link to' to connect

--- first-web/src/App.js
+++ first-web/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
-    <div className="App">
-        <Navbar />
-        <div className="content">
-            <Home />
+    <Router>
+        <div className="App">
+            <Navbar />
+            <div className="content">
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/create" element={<Create/>}/>
+                </Routes>
+            </div>
         </div>
-    </div>
+    </Router>
   );
 }
 
